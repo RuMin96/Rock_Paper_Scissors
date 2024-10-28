082d7201b03501966fff221aabe3a90b23a47890
PC <PC@DESKTOP-OR1E14N>
2024-10-28 17:11:32 +0700

    Добавление картинок , написание кода и его исправление

--- Rock_Paper_Scissors/Form1.cs
+++ Rock_Paper_Scissors/Form1.cs
@@ -12,14 +12,28 @@ namespace Rock_Paper_Scissors
 {
     public partial class Form1 : Form
     {
+        public int rounds = 3;
+        public int timePerRound = 6; 
+        string[] AIchoice = { "rock", "paper", "scissor", "rock", "scissor", "paper" };
+        public int randomNumber;
+        string command;
+        Random rnd = new Random();
+        string playerChoice;
+        int playerWins = 0;
+        int AIwins = 0;
+
         public Form1()
         {
             InitializeComponent();
+            timer1.Enabled = true;
+            playerChoice = "none";
         }
 
-        private void label6_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
     }
+   
+
 }
